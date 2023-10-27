09b2404b0ffec04179ff2bf0365d26499c104dd9
aaru637 <dhandapanisakthi123@gmail.com>
2023-10-27 10:39:13 +0530

    Ninth Commit

--- src/com/dk/SecondLargestElement.java
+++ src/com/dk/SecondLargestElement.java
@@ -6,21 +6,21 @@ package com.dk;
 public class SecondLargestElement {
 
     private static int secondLargestElement(int... arr) {
-        int first = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            first = Math.max(first, arr[i]);
-        }
-        int second = (first == arr[0]) ? arr[1] : arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != first) {
-                second = Math.max(second, arr[i]);
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+        for(int i = 0 ; i < arr.length ; i++) {
+            if(first < arr[i]) {
+                second = first;
+                first = arr[i];
+            }
+            else if(arr[i] != first && second < arr[i]) {
+                second = arr[i];
             }
         }
         return second;
     }
 
     public static void main(String... args) {
-        int[] arr = { 12, 1, 7, 5, 3, 10 };
+        int[] arr = { 12, 1, 7, 5, 3, 10, 13 };
         System.out.println(secondLargestElement(arr));
     }
 }
